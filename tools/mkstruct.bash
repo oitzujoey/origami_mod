182ba0bf995688e341fe6171033713a9c6b4af8a
Joey Herguth <oitzujoey@gmail.com>
2020-07-10 21:28:15 -0700

    Add mod creation and loading system

---
+++ tools/mkstruct.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MOD=$1
+ENGINE=$2
+GAMEDIR=$3
+
+if [ "$ENGINE" = "openarena" ]
+then
+    ENGINEDIR=gamecode
+    mkdir pak
+    cp -r build/release-linux-$(uname -m)/oax/vm pak
+else
+    echo "This engine is not currently supported. Aborting."
+    exit 1
+fi
+
+exit 0
